dda6ef011299e821ddaa68dc0239cdf6c8706c9c
Ken McCormack <Ken.McCormack@myob.com>
2016-01-16 00:49:11 +1100

    Given input 'D', then charOffset should equal 3

--- spec/rspec_spec.rb
+++ spec/rspec_spec.rb
@@ -1,18 +1,43 @@
 require 'spec_helper'
 
 RSpec.describe 'Coordinate map for "D"', :type => :feature do
-  
-  @model = nil
+
+class TextGridModel
+   
+   @char_offset=0
+
+   def initialize(char)
+     @char_offset = get_char_offset(char);
+   end
+
+   def char_offset 
+     @char_offset;
+   end
+
+   def get_char_offset(char)
+     0
+   end
+
+   private :get_char_offset
+   
+end
 
   before :all do
      # todo, define model and initializ 
   end
 
-  context('Given D') do 
-    it '(0, 3) should contain a character' do
-      expect(@model.cellContainsCharacter(0,3)).to eq(true)
-    end
-  end
+  # context('Given D') do 
+  #   it '(0, 3) should contain a character' do
+  #     expect(@model.cellContainsCharacter(0,3)).to eq(true)
+  #   end
+  # end
+
+  context('Given input D') do 
+     it 'charOffset should equal 3' do
+       charOffset = TextGridModel.new('D').char_offset
+       expect(charOffset).to eq(3)
+     end
+   end
 
 
 end
